68bf95a0c0fd00b44d7f14e69ddab6f0c082c9c7
Eccentric-Harry <139021195+Eccentric-Harry@users.noreply.github.com>
2023-12-20 16:41:44 +0000

    Learning Basics 1

--- Basics/test.js
+++ Basics/test.js
@@ -1,1 +1,14 @@
 console.log("Harinadh")
+
+const accountId = 20168
+let accountEmail = "Eccentricharry@gmail.com"
+var accountName = "harry"
+accountCity = "Palasa"
+
+accountEmail= "Demo@github.com"
+accountName = "Sai"
+accountCity = "Visakhapatnam"
+
+// accountId = 1344
+
+console.table([accountEmail, accountId,accountName, accountCity])
