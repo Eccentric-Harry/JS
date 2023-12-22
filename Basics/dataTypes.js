0f6c56d4cc7dffcba2deaf7ba26cc3537288a0b4
Eccentric-Harry <139021195+Eccentric-Harry@users.noreply.github.com>
2023-12-22 18:41:48 +0000

    DataTypes Summary

--- Basics/dataTypes.js
+++ Basics/dataTypes.js
@@ -37,7 +37,7 @@ const MaxNum = Number.MAX_SAFE_INTEGER
 PRIMITIVE:
 number => The Number type is a double-precision 64-bit binary format IEEE 754 value. It is capable of storing positive floating-point numbers between 2-1074 (Number.MIN_VALUE) and 21024 (Number.MAX_VALUE) as well as negative floating-point numbers between -2-1074 and -21024, but it can only safely store integers in the range -(253 − 1) (Number.MIN_SAFE_INTEGER) to 253 − 1 (Number.MAX_SAFE_INTEGER) 
 
-bigint => bas bade integers ke liye use karte hai. it can easyly store numbers that fall out of range of Max_Safe_Integer and same for Min
+bigint => bas bade integers ke liye use karte hai. it can easyly store numbers that fall out of range of Max_Safe_Integer and same for Min. // number ke peeche n laga dena.
 
 string=> ""
 boolean => true/false
@@ -52,3 +52,31 @@ objects
 console.table(typeof MaxNum);
 console.table(typeof null);  // the output is object. some say that its a language error.
 console.table(typeof undefined); // output is undefined.
+
+
+
+// data types are generally divided in two types primitive and non primitive (call by value, call by reference)
+// this differentiation is done on the basis jitna bhi data hai wo ham kis tarike se memory pe stire karte hai aur access kiya jaate hai.
+
+// JS is Dynamially typed Language
+
+const id = Symbol('123')
+const id_ = Symbol('123') //(return type = symbol)
+
+console.log(id === id_);  // false 
+
+const heros = ["SpiderMan" , "BatMan" , "Venom"]
+let myObj = {
+    name: "Harry",
+    age: 19
+}
+
+// in JS, a function can also be declared as a variable;
+let myFun = function()
+{
+    console.log("Hello World!");
+}
+
+// return types :       Arrays  =>  object
+    //    Function  =>  function
+    //    Object  =>  object
