38b468aee3e40ff76e5505397f9f946dff3221cb
Eccentric-Harry <139021195+Eccentric-Harry@users.noreply.github.com>
2023-12-24 16:09:58 +0000

    Revision

--- Basics/comparison.js
+++ Basics/comparison.js
@@ -5,12 +5,16 @@ console.log(typeof(conNum));
 
 console.log(2 > 1);
 
-console.log("2" > 1);
-console.log("02" > 1);
+console.log("2" > 1); // true
+console.log("02" > 1); //true
 
-console.log(null > 0); //true
-console.log(null == 0);//true (NaN pe convert karta hai)
+console.log(null > 0); //false
+console.log(null < 0); //false
+console.log(null == 0);//false (NaN pe convert karta hai)
 console.log(null >= 0); // true 
+console.log(null <=0); //true
+console.log(null == undefined); //true
+
 // the reason is that equality comparisons and less than or greater than work differently. less than or greater than converts the null value to int and then comparison.
 
 // all these comparisons have no sertainity ie. unpredictable outputs.
@@ -24,3 +28,5 @@ console.log(undefined >= 0); // false
 
 console.log("2" == 2); // true
 console.log("2" === 2); // false
+
+console.log(null === undefined); //false
